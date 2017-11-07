6ba07d602da1efd062a3b3497ae010bb0639e81c
Andrew Smith <andrew.smith@iovation.com>
2017-11-07 10:41:52 -0800

    Added Json code to handle headers body email and passwords and URLs using Alamofire

--- Smack/Smack/Controller/ChannelVC.swift
+++ Smack/Smack/Controller/ChannelVC.swift
@@ -12,7 +12,9 @@ class ChannelVC: UIViewController {
 
     //outlets
     @IBOutlet weak var loginButton: UIButton!
-    
+    @IBAction func prepareForUnwind(segue: UIStoryboardSegue) {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
--- Smack/Smack/Utilities/Constants.swift
+++ Smack/Smack/Utilities/Constants.swift
@@ -7,5 +7,20 @@
 //
 
 import Foundation
+
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+//URL
+let BASE_URL = "https://smacka-appa.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
 //Segues
 let TO_LOGIN = "toLogin"
+let TO_CREATE_ACCOUNT = "toCreateAccount"
+
+let UNWIND = "unwindToChannel"
+
+//user defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
